ac8782df95855fe40ad77f5f786b48786807f09a
HAKJOON0223 <2hakjoon@gmail.com>
2022-06-23 16:51:54 +0900

    add ContentLogo with Wrapper

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,27 @@
+import styled from 'styled-components'
+
 import './App.css'
+import ContentLogo from './components/ContentLogo'
+
+const MainWrapper = styled.main`
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`
+const SectionWrapper = styled.section`
+  min-width: 1200px;
+`
 
 function App() {
-  return <div className="App" />
+  return (
+    <MainWrapper>
+      <SectionWrapper>
+        <ContentLogo />
+      </SectionWrapper>
+    </MainWrapper>
+  )
 }
 
 export default App
