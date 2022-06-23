77b920287f814eb62fd233aef92e93716416e3ea
HAKJOON0223 <2hakjoon@gmail.com>
2022-06-23 16:19:17 +0900

    Setting eslint and prettier

--- src/images/index.ts
+++ src/images/index.ts
@@ -2,6 +2,6 @@ import playstore from './play-store2x.png'
 import appstore from './badge-apple4x.png'
 import triple from './triple2x.png'
 
-export const PLAYSTORE_BADGE = playstore;
-export const APPSTORE_BADGE = appstore;
+export const PLAYSTORE_BADGE = playstore
+export const APPSTORE_BADGE = appstore
 export const TRIPLE_TROPHY = triple
